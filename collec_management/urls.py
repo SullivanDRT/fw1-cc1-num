261b62effd5f8dd00bca75336c19e49be2c94c42
thomas <thomasfoucher2@gmail.com>
2024-10-30 23:02:23 +0100

    suppression des collections implémenté

--- collec_management/urls.py
+++ collec_management/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('new/', views.ajouter_collec, name="ajouter_collec")
+    path("about/", views.about, name="about"),
+    path("new/", views.ajouter_collec, name="ajouter_collec"),
+    path("delete/<int:collec_id>", views.supprimmer_collec, name="supprimmer_collec"),
+    path(
+        "delete_comfirm/<int:collec_id>",
+        views.comfirmation_suppression_collec,
+        name="comfirmation_suppression_collec",
+    ),
 ]
--- collec_management/views.py
+++ collec_management/views.py
@@ -1,25 +1,56 @@
 from django.shortcuts import render, redirect
+from .models import Collec
 from .forms import CollecForm
 from django.utils import timezone
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 
 # Create your views here.
 
 
 def about(request):
-    return render(request, 'collec_management/about.html')
+    return render(request, "collec_management/about.html")
 
 
 def ajouter_collec(request):
     if request.method == "POST":
         form = CollecForm(request.POST)
         if form.is_valid():
-            collection = form.save(commit= False)
+            collection = form.save(commit=False)
             collection.date = timezone.now()
             collection.save()
-            return HttpResponse("La collection s'est bien ajoutée à la bd, redirection faire la fiche détails à faire")
+            return HttpResponse(
+                "La collection s'est bien ajoutée à la bd, redirection faire la fiche détails à faire"
+            )
     else:
         form = CollecForm()
-    context = {'form': form}
-    return render(request, 'collec_management/ajouter_collec.html', context)
+    context = {"form": form}
+    return render(request, "collec_management/ajouter_collec.html", context)
+
+
+def supprimmer_collec(request, collec_id):
+    if request.method == "POST":
+        try:
+            collec = Collec.objects.get(pk=collec_id)
+        except Collec.DoesNotExist:
+            raise Http404("la collection n'existe pas")
+        collec.delete()
+        return HttpResponseRedirect("/about")
+    else:
+        return HttpResponseRedirect(f"/delete_comfirm/{collec_id}")
+
+
+def comfirmation_suppression_collec(request, collec_id):
+    try:
+        collec = Collec.objects.get(pk=collec_id)
+    except Collec.DoesNotExist:
+        raise Http404("la collection n'existe pas")
+    if request.method == "POST":
+        collec.delete()
+        return HttpResponseRedirect("/about")
+    else:
+        return render(
+            request,
+            "collec_management/comfirmation_suppression_collec.html",
+            {"collec": collec},
+        )
