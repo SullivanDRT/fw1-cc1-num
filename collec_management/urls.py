0e58ac11a2e6752743afc44a440b5ac7aa473022
SullivanDRT <demaret.sullivan@yahoo.com>
2024-10-30 17:15:24 +0100

    Fonctionnalité détails d'une collection terminée

--- collec_management/urls.py
+++ collec_management/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('collection/<int:id_collec>/', views.collec_details, name = 'collec_dtails'),
 ]
